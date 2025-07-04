112fc727486d1bdde406ea8450360459e7cfbcfe
blackwidow5067 <blackwidow5067@gmail.com>
2025-07-04 12:13:20 +0530

    Update StudentController.java

--- src/main/java/com/wipro/controller/StudentController.java
+++ src/main/java/com/wipro/controller/StudentController.java
@@ -2,7 +2,6 @@ package com.wipro.controller;
  
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
  
